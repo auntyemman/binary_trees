3e5e264917db9493a50c7cf1ab5b822521b0cc9c
Adedayo Adepoju <1mole.coco@gmail.com>
2022-08-23 19:42:55 +0100

    Initial commit

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -1,19 +1,25 @@
 #include "binary_trees.h"
 
 /**
- * bst_insert -
+ * bst_search - searches for a value in a binary search tree
+ * @tree: pointer to the root node of the BST to search
+ * @value: value to search in the tree
  *
- * Return:
+ * Return: pointer to the node containing a value equal to value, or NULL
  */
 bst_t *bst_search(const bst_t *tree, int value)
 {
-if (tree == NULL)
-return (NULL);
-if (tree->n == value)
-return ((void *)tree);
-if (tree->n > value)
-return (bst_search(tree->left, value));
-if (tree->n < value)
-return (bst_search(tree->right, value));
-return (NULL);
+	bst_t *tmp;
+
+	tmp = (bst_t *)tree;
+	while (tmp)
+	{
+		if (tmp->n == value)
+			return (tmp);
+		if (tmp->n < value)
+			tmp = tmp->right;
+		else
+			tmp = tmp->left;
+	}
+	return (NULL);
 }
